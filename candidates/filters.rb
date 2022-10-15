3ee23a83b6dd35220509c0fe182abc92afeafeae
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-14 21:17:50 -0600

    methods for filtering candidates

--- candidates/filters.rb
+++ candidates/filters.rb
@@ -1,16 +1,40 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+require './candidates'
 
 def find(id)
-  # Your code Here
+ for candidate in @candidates do
+  if candidate[:id] == id 
+    return candidate
+  end
+ end 
+
 end
 
 def experienced?(candidate)
-  # Your code Here
+ 
+  if candidate[:years_of_experience] > 2
+    true
+  else
+    false 
+  end
 end
 
 def qualified_candidates(candidates)
-  # Your code Here
+  q = []
+ for candidate in candidates do
+   if (experienced?(candidate) && candidate[:github_points] > 100 && candidate[:languages].include?('Ruby' || 'Python') && 
+    candidate[:date_applied] > 15.days.ago.to_date  && candidate[:age] > 17 )
+q.push candidate
+   end
+  end 
+  return q
 end
 
 # More methods will go below
+
+def ordered_by_qualifications(candidates)
+
+return candidates.sort_by {|candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+
+end
--- candidates/main.rb
+++ candidates/main.rb
@@ -6,4 +6,14 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# p experienced?@candidates[0]
+
+
+# res = find(7)
+# pp res
+
+# qa = qualified_candidates(@candidates)
+# pp qa
+
+res = ordered_by_qualifications(@candidates)
+pp res
